ba3ce20b551c7233c3f4014c2e79d76197114290
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-06 23:26:16 +0200

    get(/userid)

--- server/src/services/conversation.js
+++ server/src/services/conversation.js
@@ -3,12 +3,27 @@ import Conversation from "../models/Conversation.js";
 
 const convoRouter = Router();
 
+// initiate a convo using POST
 convoRouter.post("/", async (req, res, next) => {
   try {
     const convo = await Conversation({
       members: [req.body.senderId, req.body.receiverId],
     }).save();
     res.status(201).send(convo._id);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+});
+// GET convo of a user
+
+convoRouter.get("/:userId", async (req, res, next) => {
+  try {
+    // const convo = await Conversation.findById(req.params.userId);
+    const convo = await Conversation.find({
+      members: { $in: [req.params.userId] },
+    });
+    res.status(200).send(convo);
   } catch (error) {
     console.log(error);
     next();
