a80fb55acb6b874474535b7bc49e488fadfe1257
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-06 23:52:07 +0200

    get messege workin

--- server/src/services/message.js
+++ server/src/services/message.js
@@ -3,10 +3,24 @@ import Message from "../models/Message.js";
 
 const messageRouter = Router();
 
+// starting convo(texting) using POST
 messageRouter.post("/", async (req, res, next) => {
   try {
     const message = await Message(req.body).save();
     res.status(201).send(message);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+});
+
+//GET the message between 2
+messageRouter.get("/:convoId", async (req, res, next) => {
+  try {
+    const messages = await Message.find({
+      conversationId: req.params.convoId,
+    });
+    res.status(200).send(messages);
   } catch (error) {
     console.log(error);
     next();
