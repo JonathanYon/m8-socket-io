6ced4e9b3bc69f21b9cfb4259b6f590fce8c4b72
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-08 20:29:40 +0200

    messenger with all the fetches

--- website/src/component/messages/Messenger.jsx
+++ website/src/component/messages/Messenger.jsx
@@ -10,31 +10,69 @@ import {
 import Convo from "../conversation/Convo";
 import Chatbox from "../chat/Chatbox";
 import Online from "../chatOnline/Online";
+import { useEffect, useState } from "react";
 
 const Messenger = () => {
+  const users = ["615e0eaeb152175f1447b54d", "615e0efcb152175f1447b54f"];
+  const [convo, setConvo] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    const getConvo = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3003/convos/${users[1]}`
+        );
+        const res = await response.json();
+        console.log(res);
+        setConvo(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getConvo();
+  }, []);
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3003/messages/${currentChat._id}`
+        );
+        const res = await response.json();
+        setMessages(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMessages();
+  }, [currentChat]);
+  console.log(messages);
+
   return (
     <Container>
       <Row>
         <Col sm={3}>
           <FormControl />
-          <Convo />
-          <Convo />
-          <Convo />
-          <Convo />
+          {convo.map((convos) => (
+            <div key={convos._id} onClick={() => setCurrentChat(convos)}>
+              <Convo convo={convos} users={users[1]} />
+            </div>
+          ))}
         </Col>
         <Col sm={6} className="d-flex flex-column justify-content-between">
           <Col className="chatbox">
-            <Chatbox />
-            <Chatbox own={true} />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox own={true} />
-            <Chatbox />
-            <Chatbox />
-            <Chatbox own={true} />
-            <Chatbox />
-            <Chatbox />
+            {currentChat ? (
+              <>
+                {messages?.map((message) => (
+                  <Chatbox
+                    message={message}
+                    own={message.sender === users[1] && message.sender}
+                  />
+                ))}
+              </>
+            ) : (
+              <div>Open a new chat!💬</div>
+            )}
           </Col>
           <Col>
             <Form>
